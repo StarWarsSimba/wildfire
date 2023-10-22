c568cfaa5773f1f27b6ebfaba1d02b6c689caa89
StarWarsSimba <miles.gurney@gmail.com>
2023-10-22 12:35:55 -0700

    Added skeleton

--- wildfire.py
+++ wildfire.py
@@ -1,1 +1,13 @@
-print("hi")
+"""Geographic clustering of historical wildfire data
+CS 210, University of Oregon
+Your Name Here
+Credits: TBD
+"""
+import doctest
+
+
+def main():
+    doctest.testmod()
+
+if __name__ == "__main__":
+    main()
