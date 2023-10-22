fdb21e7717072f43c708279b7750699f1524c601
StarWarsSimba <miles.gurney@gmail.com>
2023-10-22 13:29:49 -0700

    Generated map with points

--- wildfire.py
+++ wildfire.py
@@ -4,10 +4,67 @@ Your Name Here
 Credits: TBD
 """
 import doctest
+import csv
+import config
 
+import graphics.utm_plot
+
+def make_map() -> graphics.utm_plot.Map:
+    """Create and return a basemap display"""
+    map = graphics.utm_plot.Map(config.BASEMAP_PATH,
+                                config.BASEMAP_SIZE,
+                                (config.BASEMAP_ORIGIN_EASTING, config.BASEMAP_ORIGIN_NORTHING),
+                                (config.BASEMAP_EXTENT_EASTING, config.BASEMAP_EXTENT_NORTHING))
+    return map
+
+def get_fires_utm(path: str) -> list[tuple[int, int]]:
+    """Read CSV file specified by path, returning a list
+    of (easting, northing) coordinate pairs within the
+    study area.
+
+    >>> get_fires_utm("data/test_locations_utm.csv")
+    [(442151, 4729315), (442151, 5071453), (914041, 4729315), (914041, 5071453)]
+    """
+    coord_pairs = []
+    with open(path, newline="", encoding="utf-8") as source_file:
+        reader = csv.DictReader(source_file)
+        for row in reader:
+            easting = int(row["Easting"])
+            northing = int(row["Northing"])
+            if in_bounds(easting, northing):
+                coord_pairs.append((easting, northing))
+    return coord_pairs
+
+def in_bounds(easting: float, northing: float) -> bool:
+    """Is the UTM value within bounds of the map?"""
+    if (easting < config.BASEMAP_ORIGIN_EASTING
+        or easting > config.BASEMAP_EXTENT_EASTING
+        or northing < config.BASEMAP_ORIGIN_NORTHING
+        or northing > config.BASEMAP_EXTENT_NORTHING):
+        return False
+    return True
+
+def plot_points(fire_map: graphics.utm_plot.Map,
+                points:  list[tuple[int, int]],
+                size_px: int = 5,
+                color: str = "green") -> list:
+    """Plot all the points and return a list of handles that
+    can be used for moving them.
+    """
+    symbols = []
+    for point in points:
+        easting, northing = point
+        symbol = fire_map.plot_point(easting, northing,
+                                     size_px=size_px, color=color)
+        symbols.append(symbol)
+    return symbols
 
 def main():
     doctest.testmod()
+    fire_map = make_map()
+    points = get_fires_utm(config.FIRE_DATA_PATH)
+    fire_symbols = plot_points(fire_map, points, color="red")
+    input("Press enter to quit")
 
 if __name__ == "__main__":
     main()
